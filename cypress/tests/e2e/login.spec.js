635fdd06fa218003f7f5bdb76182c25e344a3636
Adriano Santos <j.adriannosanttos@gmail.com>
2025-04-14 21:00:23 -0300

    User data Added

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,3 +1,5 @@
+const passport = require("passport");
+
 describe('Login com sucesso', () => {
   
   const selectorsList = {
@@ -8,21 +10,55 @@ describe('Login com sucesso', () => {
     firstnameFild: '#firstName',
     lastnameFild: '#lastName', 
   }
+
+  const userDate = {
+      userSuccess: {
+        username: "Heath93",
+        password: 's3cret'
+
+      },
+
+      userFail: {
+        username: 'Heath93', 
+        password: 's3crete'
+
+      }
+  }
+
+  const userRegist = {
+    registSucess: {
+      registFirstName: 'João',
+      registLastName: 'Silva',
+      registPassword: 's3cret',
+      registPasswordConfirm: 's3cret'
+    }, 
+
+    registFail: {
+      registFirstName: 'João',
+      registLastName: 'Silva',
+      registPassword: 's3cret',
+      registPasswordConfirm: 'Test'
+
+
+    }
+
+  }
   
   
   it('Deve fazer login com um usuário válido', () => {
     cy.visit('http://localhost:3000/signin')
-    cy.get(selectorsList.usernameFild).type("Heath93")
-    cy.get(selectorsList.passwordFild).type('s3cret') 
+    cy.get(selectorsList.usernameFild).type(userDate.userSuccess.username)
+    cy.get(selectorsList.passwordFild).type(userDate.userSuccess.password) 
     cy.get(selectorsList.loginButton).click()
+    
     
   });
   
      
   it('Tentarfazer login com um usuário com credenciais inválidas', () => {
     cy.visit('http://localhost:3000/signin')
-    cy.get(selectorsList.usernameFild).type('Heath93')
-    cy.get(selectorsList.passwordFild).type('s3crete')
+    cy.get(selectorsList.usernameFild).type(userDate.userFail.username)
+    cy.get(selectorsList.passwordFild).type(userDate.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get('.MuiAlert-message')
     
@@ -40,10 +76,10 @@ describe('Login com sucesso', () => {
     describe('Registro de novo usuário com sucesso', () => {
       it('Deve registrar um novo usuário com informações válidas', () => {
         cy.visit('http://localhost:3000/signup')
-        cy.get(selectorsList.firstnameFild).type('João')
-        cy.get(selectorsList.lastnameFild).type('Silva')
-        cy.get(selectorsList.passwordFild).type('s3cret')
-        cy.get(selectorsList.confirmpasswordFild).type('s3cret')
+        cy.get(selectorsList.firstnameFild).type(userRegist.registSucess.registFirstName)
+        cy.get(selectorsList.lastnameFild).type(userRegist.registSucess.registLastName)
+        cy.get(selectorsList.passwordFild).type(userRegist.registSucess.registPassword)
+        cy.get(selectorsList.confirmpasswordFild).type(userRegist.registSucess.registPasswordConfirm)
         
       });
     });
@@ -51,10 +87,10 @@ describe('Login com sucesso', () => {
     describe('Tentar registrar um novo usuário com informações incompletas', () => {
       it('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
         cy.visit('http://localhost:3000/signup')
-        cy.get(selectorsList.firstnameFild).type('João')
-        cy.get(selectorsList.lastnameFild).type('Silva')
-        cy.get(selectorsList.passwordFild).type('s3cret')
-        cy.get(selectorsList.confirmpasswordFild).type('test')
+        cy.get(selectorsList.firstnameFild).type(userRegist.registFail.registFirstName)
+        cy.get(selectorsList.lastnameFild).type(userRegist.registFail.registLastName)
+        cy.get(selectorsList.passwordFild).type(userRegist.registFail.registPassword)
+        cy.get(selectorsList.confirmpasswordFild).type(userRegist.registFail.registPasswordConfirm)
         cy.get('#confirmPassword-helper-text').contains('Password does not match')
 
       });
